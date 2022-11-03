957851e05ea72298b0547c046a44bd87d96250b8
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2022-11-03 12:28:45 -0300

    grupos de testes

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+
+
+
+it('A external test...', () => {
+
+})
+
+
+describe('Should group tests', () => {
+
+    describe('Should group more specifc tests', () => {
+
+        it('A external test...', () => {
+
+        })
+    })
+
+    it('A external test...', () => {
+
+    })
+
+})
